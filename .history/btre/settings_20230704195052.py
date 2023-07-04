169e93ffc3c6157ec8e56f2e5a47f8a2b7396fd8
florin7755 <valeanu.florin@yahoo.com>
2023-07-04 19:56:00 +0300

    Add local settings import

--- .history/btre/settings_20230704195052.py
+++ .history/btre/settings_20230704195052.py
@@ -35,12 +35,15 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -137,3 +140,21 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
+
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER='fluturele55555@gmail.com'
+EMAIL_HOST_PASSWORD='Adi999'
+EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass    
--- btre/settings.py
+++ btre/settings.py
@@ -153,3 +153,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER='fluturele55555@gmail.com'
 EMAIL_HOST_PASSWORD='Adi999'
 EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass    
